ab234b6fb2bb32994c48fa538c8b8f76b0699b7f
Walter <siposwalter98@gmail.com>
2020-11-10 15:22:56 +0200

    Final version

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println();
-
-//        Process process = Runtime.getRuntime().exec("cmd /c setx varManagerr 79");
-//        InputStream process2 = Runtime.getRuntime().exec("cmd /c echo %varManager%").getInputStream();
-//        String value =  System.getenv("varManager");
+//        System.out.println("Env before setting: " + System.getenv("varManager"));
+//
+//        Process process = Runtime.getRuntime().exec("cmd /c setx varManager 99");
+//
+//        System.out.println("Env after setting: " + System.getenv("varManager"));
 
         Manager.run();
     }
